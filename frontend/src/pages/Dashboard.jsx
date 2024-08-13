f1a68fe45f7ebd469bc2e0ff46004479dd3e3a7f
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-08-13 23:58:59 +0530

    added basic search bar with hardcoded tickers

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainChart from '../components/Dashboard_Components/MainChart'
 import CompanyProfile from '../components/Dashboard_Components/CompanyProfile'
+import TechnicalSignal from '../components/Dashboard_Components/technicalSignal'
 
 const Dashboard = () => {
   return (
     <div><MainChart />
     <CompanyProfile />
+    <TechnicalSignal />
     </div>
   )
 }
