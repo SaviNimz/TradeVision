bf120c1da70a83ff425ea754630ae254d45acb93
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-08-15 15:16:27 +0530

    final styled version 8/15

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import MainChart from '../components/Dashboard_Components/MainChart'
-import CompanyProfile from '../components/Dashboard_Components/CompanyProfile'
-import TechnicalSignal from '../components/Dashboard_Components/technicalSignal'
-import SearchBar from '../components/Dashboard_Components/searchBar'
+import React from 'react';
+import styled from 'styled-components';
+import MainChart from '../components/Dashboard_Components/MainChart';
+import CompanyProfile from '../components/Dashboard_Components/CompanyProfile';
+import TechnicalSignal from '../components/Dashboard_Components/TechnicalSignal';
+import SearchBar from '../components/Dashboard_Components/SearchBar';
 
 const Dashboard = () => {
   return (
-    <div>
-    <SearchBar />
-    <MainChart />
-    <CompanyProfile />
-    <TechnicalSignal />
-    </div>
-  )
-}
+    <Container>
+      <SearchBar />
+      <MainChart />
+      <BottomContainer>
+        <CompanyProfile />
+        <TechnicalSignal />
+      </BottomContainer>
+    </Container>
+  );
+};
 
-export default Dashboard
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 16px;
+  box-sizing: border-box;
+`;
 
+const BottomContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  margin-top: 16px;
 
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export default Dashboard;
