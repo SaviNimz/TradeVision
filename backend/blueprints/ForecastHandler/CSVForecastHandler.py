8f5d33b4faf21d08aee372ecd82d764eb48b9297
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-08-23 09:51:12 +0530

    added the forecast handler for the backend

--- backend/blueprints/ForecastHandler/CSVForecastHandler.py
+++ backend/blueprints/ForecastHandler/CSVForecastHandler.py
@@ -3,9 +3,12 @@ from flask import request, jsonify
 import os
 from werkzeug.utils import secure_filename
 from blueprints.ML_Pipeline.csv_validator import CSVValidator
+from blueprints.ML_Pipeline.models import Models
 import pandas as pd
 
 CSV_Forecast_handler = blueprints.Blueprint('csv_forecast_handler', __name__)
+models = Models()
+
 
 UPLOAD_FOLDER = 'UPLOAD_FOLDER'
 if not os.path.exists(UPLOAD_FOLDER):
@@ -13,7 +16,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 # Define the route to handle file uploads
 @CSV_Forecast_handler.route('/api/CSVupload', methods=['POST'])
-def forecast():
+def Handle_upload():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part in the request'}), 400
 
@@ -53,10 +56,43 @@ def forecast():
         
     else:
         return jsonify({'error': 'Invalid file type'}), 400
+
+
+@CSV_Forecast_handler.route('/api/forecast', methods=['POST'])
+def forecast():
+    data = request.json
+    column = data.get('column')
+
+    # Handle case sensitivity of the column name
+    column = column.capitalize() if column else column
+
+    methods = data.get('methods')
+    csv_data = data.get('csvData')  # Access the CSV data
+
+    # Convert CSV data into a DataFrame
+    df = pd.DataFrame(csv_data)
     
+    print(column)
+    results = {}
+
+    for method in methods:
+        try:
+            forecast_result = models.forecast(method, df, column)
+            
+            # Convert forecast_result to a JSON serializable format
+            if isinstance(forecast_result, pd.Series):
+                results[method] = forecast_result.to_dict()  # Convert Series to a dictionary
+            elif isinstance(forecast_result, pd.DataFrame):
+                results[method] = forecast_result.to_dict(orient='records')  # Convert DataFrame to a list of records
+            else:
+                results[method] = forecast_result  # Handle other return types directly
+
+        except Exception as e:
+            results[method] = {"error": str(e)}  # Capture any errors in the forecasting process
+
+    return jsonify(results)
 
 
 @CSV_Forecast_handler.route('/api/forecast/save')
 def save():
     return 'Save'
-
