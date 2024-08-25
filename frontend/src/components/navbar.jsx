2ebf571e33e98539d803d4237605ff53584f82c5
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-08-25 10:25:42 +0530

    fixed navbar z index

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -10,7 +10,7 @@ const NavbarContainer = styled.nav`
   background-color: #0a1e3a; /* Dark futuristic blue */
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
   position: relative;
-  z-index: 10;
+  z-index: 1;
 `;
 
 const Logo = styled.div`
