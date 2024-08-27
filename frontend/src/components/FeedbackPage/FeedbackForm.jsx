6b6c7883c8f725aaa8430f5bdaa598d39fb79b4d
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-08-27 09:25:43 +0530

    changed color theme

--- frontend/src/components/FeedbackPage/FeedbackForm.jsx
+++ frontend/src/components/FeedbackPage/FeedbackForm.jsx
@@ -18,7 +18,7 @@ const FormContainer = styled.div`
 
 const Input = styled.input`
   width: 100%;
-  padding: 10px;
+  padding: 7px;
   margin: 10px 0;
   border: 1px solid #ddd;
   border-radius: 4px;
