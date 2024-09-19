75a1a258c41e0f3bfd775fe3e6a7535baeea199b
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-09-19 21:17:30 +0530

    registered stock_forecast_handler blueprint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from blueprints.UserHandler.UserHandler import User_handler
 from blueprints.ForecastHandler.CSVForecastHandler import CSV_Forecast_handler
+from blueprints.ForecastHandler.StockForecastHandler import Stock_Forecast_handler
+
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.register_blueprint(User_handler)
 app.register_blueprint(CSV_Forecast_handler)
+app.register_blueprint(Stock_Forecast_handler)
 
 bcrypt = Bcrypt(app)
 
