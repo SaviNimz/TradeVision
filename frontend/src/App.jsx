688cb0933a7b7db5bc986b76f5c4ee09140cc164
HarithaMihimal <harithamihimal4@gmail.com>
2024-09-24 01:19:13 +0530

    Update frontend package.json ```

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
