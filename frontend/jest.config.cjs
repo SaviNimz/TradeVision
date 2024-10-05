2b728764cb33717b9e8b23cf9afd4a70b87be38d
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-10-05 22:43:10 +0530

    changed the config file to ignore css,images

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,7 +1,10 @@
 module.exports = {
-    testEnvironment: 'jsdom', 
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-  };
-  
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/mocks/fileMock.js",
+  },
+};
